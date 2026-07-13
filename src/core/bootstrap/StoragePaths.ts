73ffd649da1fb3175b535ed63800f41c83a3e2f5
aldia_mechdream <futami16237@gmail.com>
2026-07-13 16:35:15 +0800

    init

--- src/core/bootstrap/StoragePaths.ts
+++
@@ -1,7 +0,0 @@
-export const WHALLIAM_STORAGE_PATH = '.whalliam';
-
-export const LEGACY_WHALLIAM_SETTINGS_PATH = '.claudian/whalliam-settings.json';
-export const WHALLIAM_SETTINGS_PATH = `${WHALLIAM_STORAGE_PATH}/whalliam-settings.json`;
-
-export const LEGACY_SESSIONS_PATH = '.claudian/sessions';
-export const SESSIONS_PATH = `${WHALLIAM_STORAGE_PATH}/sessions`;
